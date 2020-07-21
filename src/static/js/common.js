01b7981add0853217317c1ce5b974d8889c9f43d
Aslan <berezg86@gmail.com>
2020-07-21 23:06:49 +0300

    Верстка футера

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+    $(".mob-nav__item").click(function() {
+        var $this = $(this);
+        var elem = $(this).find('.mob-nav__dropdown');
 
+        if (!$this.hasClass("is-active")) {
+            $(".mob-nav__dropdown").slideUp();
+            $(".mob-nav__item").removeClass("is-active");
+        }
+
+        $this.toggleClass("is-active");
+        elem.slideToggle();
+
+    });
+
+    $(".burger").click(function() {
+        $(this).toggleClass('is-active');
+        $('.dropdown-menu').toggleClass('is-active');
+    });
 });
