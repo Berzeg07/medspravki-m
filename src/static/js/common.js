3407102485004c08632844cc0dbcac2076fa114c
Aslan <berezg86@gmail.com>
2020-07-22 13:31:38 +0300

    Галерея филиалов

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -17,4 +17,43 @@ $(document).ready(function() {
         $(this).toggleClass('is-active');
         $('.dropdown-menu').toggleClass('is-active');
     });
+
+    var filialSlider = new Swiper('.filials-slider', {
+        slidesPerView: 4,
+        spaceBetween: 38,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next-n',
+            prevEl: '.swiper-button-prev-p',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1840: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
 });
